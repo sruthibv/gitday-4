9aceae024b7e88c6d607bc96c47563f5e60a7406
sruthibv <bsruthiuk@gmail.com>
2025-04-10 17:58:26 +0100

    Created app.py for branch

---
+++ app.py
@@ -0,0 +1,11 @@
+import boto
+
+def lambda_handler(event, context):
+  client = boto3.client('ec2')
+  response = client.run_instances(
+    ImageId='ami-0614680123427b75e',
+    InstanceType='t2.micro',
+    KeyName='devops',
+    MaxCount=1,
+    MinCount=1
+)
